fe137f8f288b1251efb59ba57d54235a2cc44fde
Om Singh <ninjaplays792020@gmail.com>
2025-03-31 12:53:43 +0530

    changed sameSite to None and secure to NODE_ENV ==="production"

--- middleware/guestsession.js
+++ middleware/guestsession.js
@@ -3,8 +3,12 @@ const cookieParser = require("cookie-parser");
 const guestsession = (req, res, next) => {
   if (!req.cookies.guest_id) {
     const guestId = Math.random().toString(36).substring(2, 9);
-    res.cookie("guest_id", guestId, { maxAge: 60 * 60 * 1000, httpOnly: false,secure: true,
-      sameSite: "Lax", });
+    res.cookie("guest_id", guestId, {
+       maxAge: 60 * 60 * 1000, 
+       httpOnly: false,
+       secure: process.env.NODE_ENV === "production",
+      sameSite: "None", 
+    });
     req.guestId = guestId;
   } else {
     req.guestId = req.cookies.guest_id;
