caafb41a1796ddc6903906f4a6f0f54ea1e933b1
Om Singh <ninjaplays792020@gmail.com>
2025-03-31 12:43:52 +0530

    changed cookie settings: secure:true and sameSite:Lax

--- middleware/guestsession.js
+++ middleware/guestsession.js
@@ -3,7 +3,8 @@ const cookieParser = require("cookie-parser");
 const guestsession = (req, res, next) => {
   if (!req.cookies.guest_id) {
     const guestId = Math.random().toString(36).substring(2, 9);
-    res.cookie("guest_id", guestId, { maxAge: 60 * 60 * 1000, httpOnly: false });
+    res.cookie("guest_id", guestId, { maxAge: 60 * 60 * 1000, httpOnly: false,secure: true,
+      sameSite: "Lax", });
     req.guestId = guestId;
   } else {
     req.guestId = req.cookies.guest_id;
