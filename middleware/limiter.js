3ebed13f545b9c3ed7add19c2e53b5df79945eb6
Om Singh <ninjaplays792020@gmail.com>
2025-03-31 12:24:58 +0530

    made limiter value to 10 to allow upto 10 api calls

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5, // Allow max 5 requests per IP
+  max: 10, // Allow max 5 requests per IP
   handler: (req, res) => {
     res.status(429).json({
       success: false,
