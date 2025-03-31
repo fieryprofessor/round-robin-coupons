c5404a1a69b5c2b8837365d37dd4090f582c4ee3
Om Singh <ninjaplays792020@gmail.com>
2025-03-31 12:31:42 +0530

    made hhtpOnly in the cookie code to false

--- middleware/guestsession.js
+++ middleware/guestsession.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const guestsession = (req, res, next) => {
   if (!req.cookies.guest_id) {
     const guestId = Math.random().toString(36).substring(2, 9);
-    res.cookie("guest_id", guestId, { maxAge: 60 * 60 * 1000, httpOnly: true });
+    res.cookie("guest_id", guestId, { maxAge: 60 * 60 * 1000, httpOnly: false });
     req.guestId = guestId;
   } else {
     req.guestId = req.cookies.guest_id;
